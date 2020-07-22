94c8f5da0cb7d45ca5077bbe78c489a2d1e6a0e2
605258778 <605258778@qq.com>
2020-07-22 09:40:17 +0800

    update：dll

--- GISData/Common/ConvertPivotTable.cs
+++ GISData/Common/ConvertPivotTable.cs
@@ -157,156 +157,156 @@ namespace GISData.Common
         /// <param name="strFileName">Excel的保存路径及名称</param>   
         public void DataTabletoExcel(System.Data.DataTable tmpDataTable, string strFileName)
         {
-            if (tmpDataTable == null)
-            {
-                return;
-            }
-            long rowNum = tmpDataTable.Rows.Count;//行数   
-            int columnNum = tmpDataTable.Columns.Count;//列数   
-            Excel.Application m_xlApp = new Excel.Application();
-            m_xlApp.DisplayAlerts = false;//不显示更改提示   
-            m_xlApp.Visible = false;
+            //if (tmpDataTable == null)
+            //{
+            //    return;
+            //}
+            //long rowNum = tmpDataTable.Rows.Count;//行数   
+            //int columnNum = tmpDataTable.Columns.Count;//列数   
+            //Excel.Application m_xlApp = new Excel.Application();
+            //m_xlApp.DisplayAlerts = false;//不显示更改提示   
+            //m_xlApp.Visible = false;
 
-            Excel.Workbooks workbooks = m_xlApp.Workbooks;
-            Excel.Workbook workbook = workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
-            Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets[1];//取得sheet1   
+            //Excel.Workbooks workbooks = m_xlApp.Workbooks;
+            //Excel.Workbook workbook = workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
+            //Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets[1];//取得sheet1   
 
-            try
-            {
-                if (rowNum > 65536)//单张Excel表格最大行数   
-                {
-                    long pageRows = 65535;//定义每页显示的行数,行数必须小于65536   
-                    int scount = (int)(rowNum / pageRows);//导出数据生成的表单数   
-                    if (scount * pageRows < rowNum)//当总行数不被pageRows整除时，经过四舍五入可能页数不准   
-                    {
-                        scount = scount + 1;
-                    }
-                    for (int sc = 1; sc <= scount; sc++)
-                    {
-                        if (sc > 1)
-                        {
-                            object missing = System.Reflection.Missing.Value;
-                            worksheet = (Excel.Worksheet)workbook.Worksheets.Add(
-                                        missing, missing, missing, missing);//添加一个sheet   
-                        }
-                        else
-                        {
-                            worksheet = (Excel.Worksheet)workbook.Worksheets[sc];//取得sheet1   
-                        }
-                        string[,] datas = new string[pageRows + 1, columnNum];
+            //try
+            //{
+            //    if (rowNum > 65536)//单张Excel表格最大行数   
+            //    {
+            //        long pageRows = 65535;//定义每页显示的行数,行数必须小于65536   
+            //        int scount = (int)(rowNum / pageRows);//导出数据生成的表单数   
+            //        if (scount * pageRows < rowNum)//当总行数不被pageRows整除时，经过四舍五入可能页数不准   
+            //        {
+            //            scount = scount + 1;
+            //        }
+            //        for (int sc = 1; sc <= scount; sc++)
+            //        {
+            //            if (sc > 1)
+            //            {
+            //                object missing = System.Reflection.Missing.Value;
+            //                worksheet = (Excel.Worksheet)workbook.Worksheets.Add(
+            //                            missing, missing, missing, missing);//添加一个sheet   
+            //            }
+            //            else
+            //            {
+            //                worksheet = (Excel.Worksheet)workbook.Worksheets[sc];//取得sheet1   
+            //            }
+            //            string[,] datas = new string[pageRows + 1, columnNum];
 
-                        for (int i = 0; i < columnNum; i++) //写入字段   
-                        {
-                            datas[0, i] = tmpDataTable.Columns[i].Caption;//表头信息   
-                        }
-                        //Excel.Range range = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[1, columnNum]);
-                        Excel.Range range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[1, columnNum]];
-                        range.Interior.ColorIndex = 15;//15代表灰色   
-                        range.Font.Bold = true;
-                        range.Font.Size = 9;
+            //            for (int i = 0; i < columnNum; i++) //写入字段   
+            //            {
+            //                datas[0, i] = tmpDataTable.Columns[i].Caption;//表头信息   
+            //            }
+            //            //Excel.Range range = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[1, columnNum]);
+            //            Excel.Range range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[1, columnNum]];
+            //            range.Interior.ColorIndex = 15;//15代表灰色   
+            //            range.Font.Bold = true;
+            //            range.Font.Size = 9;
 
-                        int init = int.Parse(((sc - 1) * pageRows).ToString());
-                        int r = 0;
-                        int index = 0;
-                        int result;
-                        if (pageRows * sc >= rowNum)
-                        {
-                            result = (int)rowNum;
-                        }
-                        else
-                        {
-                            result = int.Parse((pageRows * sc).ToString());
-                        }
+            //            int init = int.Parse(((sc - 1) * pageRows).ToString());
+            //            int r = 0;
+            //            int index = 0;
+            //            int result;
+            //            if (pageRows * sc >= rowNum)
+            //            {
+            //                result = (int)rowNum;
+            //            }
+            //            else
+            //            {
+            //                result = int.Parse((pageRows * sc).ToString());
+            //            }
 
-                        for (r = init; r < result; r++)
-                        {
-                            index = index + 1;
-                            for (int i = 0; i < columnNum; i++)
-                            {
-                                object obj = tmpDataTable.Rows[r][tmpDataTable.Columns[i].ToString()];
-                                datas[index, i] = obj == null ? "" : "'" + obj.ToString().Trim();//在obj.ToString()前加单引号是为了防止自动转化格式   
-                            }
-                            System.Windows.Forms.Application.DoEvents();
-                            //添加进度条   
-                        }
+            //            for (r = init; r < result; r++)
+            //            {
+            //                index = index + 1;
+            //                for (int i = 0; i < columnNum; i++)
+            //                {
+            //                    object obj = tmpDataTable.Rows[r][tmpDataTable.Columns[i].ToString()];
+            //                    datas[index, i] = obj == null ? "" : "'" + obj.ToString().Trim();//在obj.ToString()前加单引号是为了防止自动转化格式   
+            //                }
+            //                System.Windows.Forms.Application.DoEvents();
+            //                //添加进度条   
+            //            }
 
-                        Excel.Range fchR = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[index + 1, columnNum]];
-                        fchR.Value2 = datas;
-                        worksheet.Columns.EntireColumn.AutoFit();//列宽自适应。   
-                        m_xlApp.WindowState = Excel.XlWindowState.xlMaximized;//Sheet表最大化   
-                        //range = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[index + 1, columnNum]);
-                        range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[index + 1, columnNum]];
+            //            Excel.Range fchR = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[index + 1, columnNum]];
+            //            fchR.Value2 = datas;
+            //            worksheet.Columns.EntireColumn.AutoFit();//列宽自适应。   
+            //            m_xlApp.WindowState = Excel.XlWindowState.xlMaximized;//Sheet表最大化   
+            //            //range = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[index + 1, columnNum]);
+            //            range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[index + 1, columnNum]];
                         
-                        //range.Interior.ColorIndex = 15;//15代表灰色   
-                        range.Font.Size = 9;
-                        range.RowHeight = 14.25;
-                        range.Borders.LineStyle = 1;
-                        range.HorizontalAlignment = 1;
-                    }
-                }
-                else
-                {
-                    string[,] datas = new string[rowNum + 1, columnNum];
-                    for (int i = 0; i < columnNum; i++) //写入字段   
-                    {
-                        datas[0, i] = tmpDataTable.Columns[i].Caption;
-                    }
-                    //Excel.Range range = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[1, columnNum]);
-                    Excel.Range range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[1, columnNum]];
-                    range.Interior.ColorIndex = 15;//15代表灰色   
-                    range.Font.Bold = true;
-                    range.Font.Size = 9;
+            //            //range.Interior.ColorIndex = 15;//15代表灰色   
+            //            range.Font.Size = 9;
+            //            range.RowHeight = 14.25;
+            //            range.Borders.LineStyle = 1;
+            //            range.HorizontalAlignment = 1;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        string[,] datas = new string[rowNum + 1, columnNum];
+            //        for (int i = 0; i < columnNum; i++) //写入字段   
+            //        {
+            //            datas[0, i] = tmpDataTable.Columns[i].Caption;
+            //        }
+            //        //Excel.Range range = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[1, columnNum]);
+            //        Excel.Range range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[1, columnNum]];
+            //        range.Interior.ColorIndex = 15;//15代表灰色   
+            //        range.Font.Bold = true;
+            //        range.Font.Size = 9;
 
-                    int r = 0;
-                    for (r = 0; r < rowNum; r++)
-                    {
-                        for (int i = 0; i < columnNum; i++)
-                        {
-                            object obj = tmpDataTable.Rows[r][tmpDataTable.Columns[i].ToString()];
-                            datas[r + 1, i] = obj == null ? "" : "'" + obj.ToString().Trim();//在obj.ToString()前加单引号是为了防止自动转化格式   
-                        }
-                        System.Windows.Forms.Application.DoEvents();
-                        //添加进度条   
-                    }
-                    //Excel.Range fchR1 = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[rowNum + 1, columnNum]);
-                    Excel.Range fchR = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[rowNum + 1, columnNum]];
-                    fchR.Value2 = datas;
-                    worksheet.Columns.EntireColumn.AutoFit();//列宽自适应。   
-                    m_xlApp.WindowState = Excel.XlWindowState.xlMaximized;
+            //        int r = 0;
+            //        for (r = 0; r < rowNum; r++)
+            //        {
+            //            for (int i = 0; i < columnNum; i++)
+            //            {
+            //                object obj = tmpDataTable.Rows[r][tmpDataTable.Columns[i].ToString()];
+            //                datas[r + 1, i] = obj == null ? "" : "'" + obj.ToString().Trim();//在obj.ToString()前加单引号是为了防止自动转化格式   
+            //            }
+            //            System.Windows.Forms.Application.DoEvents();
+            //            //添加进度条   
+            //        }
+            //        //Excel.Range fchR1 = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[rowNum + 1, columnNum]);
+            //        Excel.Range fchR = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[rowNum + 1, columnNum]];
+            //        fchR.Value2 = datas;
+            //        worksheet.Columns.EntireColumn.AutoFit();//列宽自适应。   
+            //        m_xlApp.WindowState = Excel.XlWindowState.xlMaximized;
 
-                    //range = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[rowNum + 1, columnNum]);
-                    range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[rowNum + 1, columnNum]];
-                    //range.Interior.ColorIndex = 15;//15代表灰色   
-                    range.Font.Size = 9;
-                    range.RowHeight = 14.25;
-                    range.Borders.LineStyle = 1;
-                    range.HorizontalAlignment = 1;
-                }
-                workbook.Saved = true;
-                //Create a workbook
-                Excel.Workbook workbookCopy = new Excel.Workbook();
+            //        //range = worksheet.get_Range(worksheet.Cells[1, 1], worksheet.Cells[rowNum + 1, columnNum]);
+            //        range = worksheet.Range[worksheet.Cells[1, 1], worksheet.Cells[rowNum + 1, columnNum]];
+            //        //range.Interior.ColorIndex = 15;//15代表灰色   
+            //        range.Font.Size = 9;
+            //        range.RowHeight = 14.25;
+            //        range.Borders.LineStyle = 1;
+            //        range.HorizontalAlignment = 1;
+            //    }
+            //    workbook.Saved = true;
+            //    //Create a workbook
+            //    Excel.Workbook workbookCopy = new Excel.Workbook();
 
-                //Load an excel file including pivot table
-                Excel.Application xlApp = new Excel.Application();
-                xlApp.DisplayAlerts = false;
-                xlApp.Visible = false;
-                xlApp.ScreenUpdating = false;
-                Excel.Workbook xlsWorkBook = xlApp.Workbooks.Open(strFileName, System.Type.Missing, System.Type.Missing, System.Type.Missing,
-                    System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing,
-                    System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing);
-                Excel.Worksheet sheet = xlsWorkBook.Worksheets["数据表"];
-                sheet.Copy(worksheet);
-                //workbook.SaveCopyAs(strFileName);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("导出异常：" + ex.Message, "导出异常", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            finally
-            {
-                EndReport();
-                //setpivottable(@strFileName, "数据表", "透视表");
-            }
+            //    //Load an excel file including pivot table
+            //    Excel.Application xlApp = new Excel.Application();
+            //    xlApp.DisplayAlerts = false;
+            //    xlApp.Visible = false;
+            //    xlApp.ScreenUpdating = false;
+            //    Excel.Workbook xlsWorkBook = xlApp.Workbooks.Open(strFileName, System.Type.Missing, System.Type.Missing, System.Type.Missing,
+            //        System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing,
+            //        System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing);
+            //    Excel.Worksheet sheet = xlsWorkBook.Worksheets["数据表"];
+            //    sheet.Copy(worksheet);
+            //    //workbook.SaveCopyAs(strFileName);
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("导出异常：" + ex.Message, "导出异常", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //}
+            //finally
+            //{
+            //    EndReport();
+            //    //setpivottable(@strFileName, "数据表", "透视表");
+            //}
         }
 
         /// <summary>   
